f82b16a25fa5835abd5d236b79e977c5ad6e30e1
CapriceLoren <capriceloren@gmail.com>
2022-10-04 13:12:59 -0400

    add score

--- script.js
+++ script.js
@@ -1,14 +1,71 @@
 const url = "https://opentdb.com/api.php?amount=10"
+let cards 
+const triviaContainer = document.querySelector(".triviaContainer");
+console.log(typeof triviaContainer)
+let score = 0
+let card = document.querySelector(".child")
+let counter = 0
+let scoreDiv = document.querySelector(".scoreDiv")
 
 fetch(url)
   .then((res) => res.json())
   .then((res) => {
-    // console.log(res.results)
-    triviaCard(res.results)
+    // console.log(res.results[0].category)
+    cards = res.results
+    for (let i = 0; i < res.results.length; i++){
+      triviaCard()
+    }
+
+    function triviaCard() {
+
+      let htmlTemplate = `
+          <div class="template">
+              <h1>${cards[0].category}</h1>
+              <p>Question: ${cards[counter].question}</p>
+              <button class="correct">${cards[counter].correct_answer}<button>
+              <button>${cards[counter].incorrect_answers[0]}</button>
+              <button>${cards[counter].incorrect_answers[1]}</button>
+              <button>${cards[counter].incorrect_answers[2]}</button>
+          </div>
+          `;
+    
+      
+      triviaContainer.removeChild(triviaContainer.firstElementChild)
+      triviaContainer.insertAdjacentHTML("beforeend", htmlTemplate);
+    }
+
+    // function counterInt() {
+    //   counter++
+    // }
+      
+    document.addEventListener("click", e => {
+      if (e.target.matches(".correct")) {
+        score++
+        scoreDiv.innerHTML = `Score: ${score}`
+        // this.classList.add("green")
+      } else {
+        // this.classList.add("red")
+      }
+
+      // counter++
+      // triviaCard()
+
     })
+
+    document.addEventListener("click", e => {
+      if (e.target.matches(".next")) {
+
+        counter++
+        triviaCard()
+      }
+    })
+  })
+
   .catch((error) => {
     console.error("Something went wrong...", error);
   });
+
+  // console.log(score)
 
   //push api data into trivia card
   //put answers into buttons
@@ -19,25 +76,4 @@ fetch(url)
   //gold tier: skip question, track score even on reload
 
 
-const triviaContainer = document.querySelector(".triviaContainer");
 
-
-
-function triviaCard(arr) {
-  console.log(arr)
-  arr.forEach((data) => {
-    let htmlTemplate = `
-        <h1>${data.category}</h1>
-        <p>Question: ${data.question}</p>
-        <button>${data.correct_answer}<button>
-        <button>${data.incorrect_answers[0]}</button>
-        <button>${data.incorrect_answers[1]}</button>
-        <button>${data.incorrect_answers[2]}</button>
-      </div>
-    `;
-
-  triviaContainer.insertAdjacentHTML("afterend", htmlTemplate);
-});
-}
-
-// triviaCard()
